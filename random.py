30c4eeda44bc90a5f9a4640c4ead86a5643f4d6e
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-11-22 16:52:36 +0200

    Update random.py

--- random.py
+++ random.py
@@ -19,7 +19,7 @@ if data_file is not None:
     colnames = ['Decoder Number']
     df = pd.read_csv(data_file,names=colnames)
     df = df.sample(n=30).reset_index().rename({'index':'Column Number'}, axis = 'columns')
-    df.index = df.index + 2
+    df.index = df.index + 1
     st.dataframe(df, width=500, height=1000)
   else:
     st.write('Thank You For Trusting Us')
